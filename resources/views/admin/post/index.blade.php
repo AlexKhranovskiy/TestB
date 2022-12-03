0b5befc362a7586acdf833d38baf2292cd4b75ed
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-12-03 19:24:13 +0200

    Created model, migration, factory, seeder for Emploee

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <div class="container">
-        <table class="table" id="employees">
+        <table class="table table-striped projects" id="employees">
             <thead>
             <tr>
                 <th scope="col">#</th>
+                <th scope="col">Avatar</th>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
                 <th scope="col">Varified at</th>
@@ -16,6 +17,13 @@
             @foreach($users as $user)
                 <tr>
                     <th scope="row">{{$user->id}}</th>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                        </ul>
+                    </td>
                     <td>{{$user->name}}</td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->email_verified_at}}</td>
