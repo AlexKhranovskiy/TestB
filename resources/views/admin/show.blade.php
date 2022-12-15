113795102d3a33e9db104dd9f069379f06cf78c2
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-12-15 19:44:23 +0200

    Fixed DatePicker

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -22,75 +22,24 @@
                                    placeholder="{{$data['author_email']}}">
                         </div>
                         <div class="form-group">
+                            <label>Employment date:</label>
+                            <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                                <input type="text" placeholder="{{$data['employment_date']}}" class="form-control datetimepicker-input"
+                                       data-target="#reservationdate"/>
+                                <div class="input-group-append" data-target="#reservationdate"
+                                     data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
                             <label for=""></label>
                             <input type="email" class="form-control" id="" placeholder="">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="card-body">
-                            <!-- Date -->
-                            <div class="form-group">
-                                <label>Date:</label>
-                                <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
-                                    <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Date and time -->
-                            <div class="form-group">
-                                <label>Date and time:</label>
-                                <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
-                                    <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
-                                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.form group -->
-                            <!-- Date range -->
-                            <div class="form-group">
-                                <label>Date range:</label>
 
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="far fa-calendar-alt"></i>
-                      </span>
-                                    </div>
-                                    <input type="text" class="form-control float-right" id="reservation">
-                                </div>
-                                <!-- /.input group -->
-                            </div>
-                            <!-- /.form group -->
-
-                            <!-- Date and time range -->
-                            <div class="form-group">
-                                <label>Date and time range:</label>
-
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="far fa-clock"></i></span>
-                                    </div>
-                                    <input type="text" class="form-control float-right" id="reservationtime">
-                                </div>
-                                <!-- /.input group -->
-                            </div>
-                            <!-- /.form group -->
-
-                            <!-- Date and time range -->
-                            <div class="form-group">
-                                <label>Date range button:</label>
-
-                                <div class="input-group">
-                                    <button type="button" class="btn btn-default float-right" id="daterange-btn">
-                                        <i class="far fa-calendar-alt"></i> Date range picker
-                                        <i class="fas fa-caret-down"></i>
-                                    </button>
-                                </div>
-                            </div>
-                            <!-- /.form group -->
                         </div>
                     </div>
                 </div>
